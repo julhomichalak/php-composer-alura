455e910da92173d30d56e23c406797013a44481c
Julho Michalak <julhomichalak@gmail.com>
2023-07-24 17:42:51 -0300

    fix

--- buscar-cursos.php
+++ buscar-cursos.php
@@ -9,7 +9,7 @@ use Symfony\Component\DomCrawler\Crawler;
 
 $client = new Client([
     'base_uri' => 'https://www.alura.com.br/',
-    'verify' => false //Não faz verificação de domínio
+    'verify' => false
 ]);
 $crawler = new Crawler();
 
